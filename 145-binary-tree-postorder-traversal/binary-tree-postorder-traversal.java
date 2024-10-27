b93eea56c92d1a69f0ee10e0ebc9d7a2caaea412
Sreeram  Bangaru <36356070+reysree@users.noreply.github.com>
2024-10-27 16:48:27 -0400

    Time: 0 ms (100.00%) | Memory: 41.3 MB (91.32%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        ArrayList<Integer> alist = new ArrayList<>();
+        recursion(root, alist);
+        return alist;
+    }
+
+    public static void recursion(TreeNode root, ArrayList<Integer> alist){
+        if(root == null){
+            return;
+        }
+
+        recursion(root.left, alist);
+        recursion(root.right, alist);
+        alist.add(root.val);
+    }
+}
