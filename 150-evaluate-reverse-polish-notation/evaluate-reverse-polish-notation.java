5ed6a0c8dbc0d83f16bff74cb1e41c0aa9ba8615
Sreeram  Bangaru <36356070+reysree@users.noreply.github.com>
2025-04-06 00:27:54 -0400

    Time: 5 ms (97.78%) | Memory: 44.9 MB (42.73%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,43 +1,41 @@
 class Solution {
     public int evalRPN(String[] tokens) {
         
-        Stack<String> stack = new Stack<>();
-        int val1 = 0;
-        int val2 = 0;
-        int res = 0;
+        Stack<Integer> stack = new Stack<>();
+        // int val1 = 0;
+        // int val2 = 0;
+        // int res = 0;
 
         for(String s : tokens){
 
             switch(s){
                 case "+":
-                    val2 = Integer.parseInt(stack.pop());
-                    val1 = Integer.parseInt(stack.pop());
-                    res = val1+val2;
-                    stack.push(res+"");
+                    // val2 = Integer.parseInt(stack.pop());
+                    // val1 = Integer.parseInt(stack.pop());
+                    // res = val1+val2;
+                    stack.push(stack.pop() + stack.pop());
                     break;
                 case "-":
-                    val2 = Integer.parseInt(stack.pop());
-                    val1 = Integer.parseInt(stack.pop());
-                    res = val1-val2;
-                    stack.push(res+"");
+                    int sub2 = stack.pop();
+                    int sub1 = stack.pop();
+                    stack.push(sub1-sub2);
                     break;
                 case "*":
-                    val2 = Integer.parseInt(stack.pop());
-                    val1 = Integer.parseInt(stack.pop());
-                    res = val1*val2;
-                    stack.push(res+"");
+                    // val2 = Integer.parseInt(stack.pop());
+                    // val1 = Integer.parseInt(stack.pop());
+                    // res = val1*val2;
+                    stack.push(stack.pop() * stack.pop());
                     break;     
                 case "/":
-                    val2 = Integer.parseInt(stack.pop());
-                    val1 = Integer.parseInt(stack.pop());
-                    res = val1/val2;
-                    stack.push(res+""); 
+                    int divisor = stack.pop();
+                    int dividend = stack.pop();
+                    stack.push(dividend/divisor); 
                     break;  
                 default: 
-                    stack.push(s);
+                    stack.push(Integer.parseInt(s));
                     break;              
             }
         }
-        return Integer.parseInt(stack.peek());
+        return stack.peek();
     }
 }
